d813ec6ec07822ba9dc81ea50ab615bf3335e230
Hritik Srivastava <srivastavaarpit1105@gmail.com>
2026-08-27 00:15:23 +0530

    fix: use environment variable for server port in app listening

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const connectDB = require("./src/config/db")
 
 connectDB()
 
-
+const PORT = process.env.PORT || 3000;
 
 app.listen(3000, ()=>{
-    console.log("Server is running on port 3000")
+    console.log(`Server is running on port ${PORT}`)
 })
