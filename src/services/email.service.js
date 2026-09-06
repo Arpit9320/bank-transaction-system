244e3a42cd4a5f25f6cdf934327447b201ea29b3
Hritik Srivastava <srivastavaarpit1105@gmail.com>
2026-09-06 12:38:08 +0530

    fix: update nodemailer transport configuration to use SMTP settings

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -1,7 +1,10 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  family: 4,
   auth: {
     type: 'OAuth2',
     user: process.env.EMAIL_USER,
